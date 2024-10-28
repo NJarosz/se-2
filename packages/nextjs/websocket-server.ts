49945f94ea3d4def5d78ab3d5fbacdc359d37277
NJarosz <63024003+NJarosz@users.noreply.github.com>
2024-10-28 10:06:03 -0500

    Add files via upload

---
+++ packages/nextjs/websocket-server.ts
@@ -0,0 +1,24 @@
+const { WebSocketServer } = require("ws");
+
+const wss = new WebSocketServer({ host: 'localhost', port: 4000 }); // Or whichever port you want.  Be sure to set Firewall accordingly.
+
+wss.on('connection', (ws) => {
+    console.log('New client connected');
+
+    ws.on('message', (message) => {
+        // Broadcast the message to all connected clients
+        const messageString = message.toString();
+        console.log('Received:', messageString);
+        wss.clients.forEach((client) => {
+            if (client.readyState === client.OPEN) {
+                client.send(messageString);
+            }
+        });
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
+
+console.log('WebSocket server is running on ws://localhost:4000');
